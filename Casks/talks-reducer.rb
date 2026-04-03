f6392130eb9a3129b7c0427f2798ce67a75f8341
Stanislav Popov <popstas@gmail.com>
2026-04-03 17:24:26 +0500

    Add Cask definition for talks-reducer macOS app

---
+++ Casks/talks-reducer.rb
@@ -0,0 +1,15 @@
+cask "talks-reducer" do
+  version "0.10.1"
+  sha256 "c8c519c75c5e67ba935808b8940920b7c491d2dc92d19aa4258b7d1a0017660a"
+
+  url "https://github.com/popstas/talks-reducer/releases/download/v#{version}/talks-reducer-macos.app-#{version}.zip"
+  name "Talks Reducer"
+  desc "Remove silent parts from video recordings"
+  homepage "https://github.com/popstas/talks-reducer"
+
+  app "talks-reducer.app"
+
+  zap trash: [
+    "~/Library/Preferences/talks-reducer",
+  ]
+end
